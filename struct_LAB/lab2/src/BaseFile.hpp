f38d7ac28cb16536ac060229addbf0d485a06b4e
Artem <artem.epp@gmail.com>
2026-02-26 19:57:25 +0300

    need finish

--- struct_LAB/lab2/src/BaseFile.hpp
+++ struct_LAB/lab2/src/BaseFile.hpp
@@ -1,14 +1,17 @@
 #ifndef BASE
 #define BASE
 #include <cstdio> 
-class BaseFile{
+#include "IFile.hpp"
+
+class BaseFile: public IFile{
     private:
         FILE* file;
     public:
         BaseFile();
         BaseFile(const char* path, const char* mode);
         BaseFile(FILE* f);
-        ~BaseFile();
+        virtual ~BaseFile();/*Рекомендуется делать виртуальными деструкторы для того, чтобы гарантировать
+правильное освобождение памяти из-под динамического объекта,*/
 
         bool is_open();
         bool can_read();
@@ -17,10 +20,38 @@ class BaseFile{
         size_t read_raw(void *buf, size_t max_bytes);
         long tell() const;
         bool seek(long offset);
-        size_t write(const void* buf, size_t n_bytes);
-        size_t read(void* buf, size_t max_bytes);
-
+        virtual size_t write(const void* buf, size_t n_bytes);
+        virtual size_t read(void* buf, size_t max_bytes);//делаем виртуальными, чтобы переопределить
 
 };
+
+class Base32File: public BaseFile{//используем ключ доступа public, который фактически не вносит изменения в область видимости.
+    private:
+        char encode_table[33];
+    public:
+        Base32File();
+        Base32File(const char* path, const char* mode, const char* custom_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456");
+        Base32File(FILE* f, const char* custom_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456");
+        
+        int get_index(char c);
+        size_t write(const void* buf, size_t n_bytes);
+        size_t read(void* buf, size_t max_bytes);
+        virtual ~Base32File();
+};
+
+
+class RleFile: public BaseFile{
+    public:
+        RleFile();
+        RleFile(const char* path, const char* mode);
+        RleFile(FILE* f);
+        virtual ~RleFile();
+
+        size_t write(const void* buf, size_t n_bytes);//опять переопределяем относительно BasicFile
+        size_t read(void* buf, size_t max_bytes);
+};
+
+
+void write_int(IFile& f, int n);
 
 #endif
---
+++ struct_LAB/lab2/src/IFile.hpp
@@ -0,0 +1,14 @@
+#ifndef IFILE
+#define IFILE
+#include <cstdio> 
+#include <cstddef>
+class IFile {
+    public:
+        virtual bool can_read()=0;
+        virtual bool can_write()=0;
+        virtual size_t write(const void* buf, size_t n_bytes)=0;
+        virtual size_t read(void* buf, size_t max_bytes)=0;
+        virtual ~IFile()=0;
+};
+inline IFile::~IFile() {}
+#endif
